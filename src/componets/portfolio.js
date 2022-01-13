0d57b623c85810e0f5aa02f2a041080a29c189d5
Shagens <Hagensshik@gmail.com>
2022-01-12 22:08:34 -0500

    updates

---
+++ src/componets/portfolio.js
@@ -0,0 +1,24 @@
+import React from "react"
+function portfolio () {
+    return( 
+        <section id="portfolio">
+      <div className="row">
+      <div id="Work" class="Work">
+                <h3>Deployed Sites</h3>
+                <a href="https://shagens.github.io/New-To-Town/">New To Town!</a>
+              <a href="https://electroenthusiast.herokuapp.com/login">Electro Enthusiast!</a>
+              <a href="https://secret-journey-36348.herokuapp.com/">Cupid's Blind Date</a>
+                
+                <p>
+                    Some of my work!
+                </p>
+            </div>
+            </div>
+            </section>
+            );
+    
+  }
+
+
+
+        export default portfolio;
